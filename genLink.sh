9664fe4ad4bf9be40dd963ce443eb3e444387b8a
The-Hoster <43914540+The-Hoster@users.noreply.github.com>
2018-10-08 14:33:41 +0200

    initial upload

---
+++ genLink.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+YT_FILE="ytplaylist"
+
+listLink="https://www.youtube.com/watch_videos?video_ids="
+for line in $(cat $YT_FILE | sort -R); do
+	# below is the ""regex"" to extract the youtube video-id. very brittle and only works if the ID comes after the last '=' and nothing after it
+        line=${line##*=}
+        listLink=$listLink$line,
+done
+listLink=${listLink%?}
+echo $listLink
+exit 0
