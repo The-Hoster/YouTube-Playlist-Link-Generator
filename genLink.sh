7991cc08e9aa04b1e03dd664c05346d93ef6f48d
The-Hoster <43914540+The-Hoster@users.noreply.github.com>
2018-10-08 14:34:44 +0200

    Update genLink.sh

--- genLink.sh
+++ genLink.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 YT_FILE="ytplaylist"
 
+#TODO; input filtering
+
 listLink="https://www.youtube.com/watch_videos?video_ids="
 for line in $(cat $YT_FILE | sort -R); do
 	# below is the ""regex"" to extract the youtube video-id. very brittle and only works if the ID comes after the last '=' and nothing after it
